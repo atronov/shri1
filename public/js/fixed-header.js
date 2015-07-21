518a9ec7a23ce91d442a1e8b2b5eff913e10168e
Alexey Tronov <vkusny@yandex-team.ru>
2015-07-21 13:02:59 +0300

    js реализация фиксированного заголовка

--- public/js/fixed-header.js
+++ public/js/fixed-header.js
@@ -2,8 +2,18 @@
  * Created by atronov on 21.07.15.
  */
 $(document).on("ready", function() {
-    var $table = $(".flight-table");
+    var $headerRow = $(".table__row-header");
+    var $fixedHeaders = $headerRow.find(".my-cell");
     $(document).on("scroll resize", function() {
-        var top = $table.scrollTop();
+        var windowTop =  $headerRow.offset().top - $(window).scrollTop();
+        if (windowTop < 0) {
+            $fixedHeaders
+                .css("top", -windowTop+"px")
+                .css("bottom", windowTop+"px");
+        } else {
+            $fixedHeaders
+                .css("top", "0px")
+                .css("bottom", "0px");
+        }
     });
 });

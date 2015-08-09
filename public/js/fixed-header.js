3217541689bacf2b2ab616bacf6ccdd84e3992f1
atronov <atronov@gmail.com>
2015-08-09 18:08:00 +0400

    Избавился от jquery

--- public/js/fixed-header.js
+++ public/js/fixed-header.js
@@ -1,19 +1,21 @@
 /**
  * Created by atronov on 21.07.15.
  */
-$(document).on("ready", function() {
-    var $headerRow = $(".table__row-header");
-    var $fixedHeaders = $headerRow.find(".table__cell-header--fixed");
-    $(document).on("scroll resize", function() {
-        var windowTop =  $headerRow.offset().top - $(window).scrollTop();
-        if (windowTop < 0) {
-            $fixedHeaders
-                .css("top", -windowTop+"px")
-                .css("bottom", windowTop+"px");
-        } else {
-            $fixedHeaders
-                .css("top", "0px")
-                .css("bottom", "0px");
-        }
-    });
-});
+var headerRow = document.querySelector(".table__row-header");
+var fixedHeaders = document.querySelectorAll(".table__cell-header--fixed");
+var handleScroll = function() {
+    var windowTop = headerRow.getBoundingClientRect().top;
+    if (windowTop < 0) {
+        Array.prototype.forEach.call(fixedHeaders, function(header) {
+            header.style.top = -windowTop+"px";
+            header.style.bottom = windowTop+"px";
+        });
+    } else {
+        Array.prototype.forEach.call(fixedHeaders, function(header) {
+            header.style.top = "0px";
+            header.style.bottom = "0px";
+        });
+    }
+};
+document.addEventListener("scroll", handleScroll);
+document.addEventListener("resize", handleScroll);
